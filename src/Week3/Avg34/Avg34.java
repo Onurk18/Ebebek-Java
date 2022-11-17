a98c050798346718a0f20e8dddc583f77f2f55e7
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 20:53:10 +0300

    Average 3 4

---
+++ src/Week3/Avg34/Avg34.java
@@ -0,0 +1,23 @@
+package Week3.Avg34;
+
+import java.util.Scanner;
+
+public class Avg34 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int sum=0, count=0, target= input.nextInt();
+
+
+
+        for (int i=0;i<=target;i++){
+            if(i%4==0||i%3==0){
+                sum+=i;
+                count++;
+                System.out.println(count);
+            }
+        }
+        double avg=(double)sum/count;
+        System.out.println("Ortalama: "+avg);
+    }
+}
