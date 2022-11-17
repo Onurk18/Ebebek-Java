a4e460414c5fbcf275ed3f7d8212d08a64c1cc3d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-18 01:24:43 +0300

    Recursive Prime

--- src/Week3/RecPrime/RecPrime.java
+++ src/Week3/RecPrime/RecPrime.java
@@ -3,9 +3,9 @@ package Week3.RecPrime;
 import java.util.Scanner;
 
 public class RecPrime {
-    static boolean isPrime(int x,int count){
-        if(count*count<=x){
-            return x%count!=0 && isPrime(x,count+1);
+    static boolean isPrime(int x,int count){//asal kontrol metodu
+        if(count*count<=x){//a sayısının karesi b'den büyükse a, b'nin çarpanı olamaz
+            return x%count!=0 && isPrime(x,count+1);//bi üs çarpanı incele
         }
         else {
             return true;
