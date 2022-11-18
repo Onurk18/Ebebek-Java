f29712ea7be97dfc5d5c0c339be300c18c5ebdfd
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 00:22:34 +0300

    StudentSystem

---
+++ src/Week4/StudentSystem/Student.java
@@ -0,0 +1,85 @@
+package Week4.StudentSystem;
+
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya,int matsoz, int kimsoz, int fizsoz) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+        if (matsoz >= 0 && matsoz <= 100) {
+            this.mat.oralnote = matsoz;
+        }
+
+        if (fizsoz >= 0 && fizsoz <= 100) {
+            this.fizik.oralnote = fizsoz;
+        }
+
+        if (kimsoz >= 0 && kimsoz <= 100) {
+            this.kimya.oralnote = kimsoz;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = (this.fizik.note*0.8+this.fizik.oralnote*0.2 + this.kimya.note*0.8+this.kimya.oralnote*0.2 + this.mat.note*0.8+this.mat.oralnote*0.2) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+
+}
