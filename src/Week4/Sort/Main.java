3a8604e73f69122e90f2423c51757264cb830b9b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 17:35:03 +0300

    Sort

---
+++ src/Week4/Sort/Main.java
@@ -0,0 +1,34 @@
+package Week4.Sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int size;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutu : ");
+        size = input.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++){
+            System.out.print(i+1 + ". Elemani giriniz : ");
+            arr[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < size-1; i++){
+            int index = i;
+            for (int j = i+1; j < size; j++){
+                if (arr[j] < arr[index]){
+                    index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+
+        System.out.println("Siralama : "+ Arrays.toString(arr));
+    }
+}
