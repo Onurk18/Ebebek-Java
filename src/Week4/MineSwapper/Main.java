e1b81d33171450df141b32ac408076d5a374e651
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 18:49:02 +0300

    MineSweeper

---
+++ src/Week4/MineSwapper/Main.java
@@ -0,0 +1,20 @@
+package Week4.MineSwapper;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 19; i++){
+            System.out.println(FirstFactorial(i));
+        }
+        //MineSweeper m1 = new MineSweeper();
+        //m1.run();
+
+    }
+
+    public static long FirstFactorial(long num) {
+        // code goes here
+        if (num == 1)
+            return 1;
+
+        return num * (FirstFactorial(num-1));
+    }
+}
